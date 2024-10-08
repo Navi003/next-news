c6df3acee044735a546271b32c2905dd0bdab163
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-10-08 23:19:21 +0200

    added error page

--- app/archive/@archive/[[...filter]]/error.jsx
+++ app/archive/@archive/[[...filter]]/error.jsx
@@ -2,9 +2,11 @@
 
 import React from "react";
 
+// Error Page 
 export default function FilterError({ error }) {
   return (
     <div id="erro">
+      
       <h2>An Erro Occured</h2>
       <p>{error.message}</p>
     </div>
