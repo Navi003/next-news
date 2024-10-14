f585e60417acd6c4b41eae3080a072e153154d6b
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-10-14 17:03:34 +0200

    Added SQL DATA HANDLER

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
 import React from "react";
-
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/NewsList";
+import { getAllNews } from "@/lib/news";
 
-export default function page() {
+export default async function page() {
+  const news = getAllNews();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />;
     </>
   );
 }
